5dcc0892fbcf1654c513c9a48013a8127521363c
tokita777 <kevinaraujodasilva266@gmail.com>
2024-11-24 02:45:33 -0300

    Mensagem explicativa das alterações

---
+++ commands/letra.js
@@ -0,0 +1,35 @@
+const { EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    name: 'letra',
+    description: 'Busca a letra de uma música',
+    async execute(message, args) {
+        if (args.length < 2) {
+            return message.reply('Por favor, informe o nome do artista e da música. Exemplo: `!letra Imagine John Lennon`');
+        }
+
+        const artist = args[0];
+        const song = args.slice(1).join(' ');
+
+        try {
+            const response = await axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`);
+            const lyrics = response.data.lyrics;
+
+            if (!lyrics) {
+                return message.reply('Desculpe, não encontrei a letra dessa música.');
+            }
+
+            const embed = new EmbedBuilder()
+                .setColor('#1DB954')
+                .setTitle(`Letra da música: ${song}`)
+                .setDescription(lyrics.length > 4096 ? lyrics.slice(0, 4093) + '...' : lyrics)
+                .setFooter({ text: `Solicitado por ${message.author.username}`, iconURL: message.author.displayAvatarURL() });
+
+            message.channel.send({ embeds: [embed] });
+        } catch (error) {
+            console.error(error);
+            message.reply('Ocorreu um erro ao buscar a letra. Verifique o nome da música e do artista.');
+        }
+    },
+};
